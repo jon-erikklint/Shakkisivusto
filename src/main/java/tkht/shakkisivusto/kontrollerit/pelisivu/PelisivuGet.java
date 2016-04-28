2378caf2ad12b975cbd2b4d4d4c42366623cd1ae
Jon-Erik Klint <jon-erik.klint@luukku.com>
2016-04-28 18:58:07 +0300

    pelisivu lisätty

--- src/main/java/tkht/shakkisivusto/kontrollerit/pelisivu/PelisivuGet.java
+++ src/main/java/tkht/shakkisivusto/kontrollerit/pelisivu/PelisivuGet.java
@@ -19,14 +19,17 @@ public class PelisivuGet extends KirjautunutHelper{
 
     @Override
     public void handle(Request rqst, Response rspns, Map map, Pelaaja kirjautunut) throws Exception {
+        System.out.println("ALOITETTU");
         int peliid = Integer.parseInt(rqst.params(":peli"));
-        
+        System.out.println("PELIID SAATU");;
         Peli peli = peliDao.findOneRambling(peliid);
-        
+        System.out.println("PELI LÖYDETTY");
         map.put("kartta", peli.getUusinVuoro().getRuudut());
-        
+        System.out.println("PELI LISÄTTY");
         int vuorossaolijanid = peli.getUusinVuoro().getPelaajaid();
+        System.out.println("VUOROSSAOLIJA LÖYDETTY");
         map.put("pelaajanVuoro", vuorossaolijanid == kirjautunut.getIndeksi());
+        System.out.println("EHTO LISÄTTY");
     }
     
 }
