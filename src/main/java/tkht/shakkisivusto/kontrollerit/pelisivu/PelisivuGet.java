687617e4bbec667804d30f9fc1aa123888f298b2
Jon-Erik Klint <jon-erik.klint@luukku.com>
2016-05-02 14:01:36 +0300

    debug

--- src/main/java/tkht/shakkisivusto/kontrollerit/pelisivu/PelisivuGet.java
+++ src/main/java/tkht/shakkisivusto/kontrollerit/pelisivu/PelisivuGet.java
@@ -1,11 +1,11 @@
 package tkht.shakkisivusto.kontrollerit.pelisivu;
 
+import java.util.List;
 import java.util.Map;
 import spark.Request;
 import spark.Response;
 import tkht.shakkisivusto.domain.Pelaaja;
 import tkht.shakkisivusto.domain.Peli;
-import tkht.shakkisivusto.kontrollerit.KirjautunutHelper;
 import tkht.shakkisivusto.kontrollerit.SessionManager;
 import tkht.shakkisivusto.tietokanta.PeliDao;
 
@@ -23,6 +23,16 @@ public class PelisivuGet extends PeliHelper{
     public void handle(Request rqst, Response rspns, Map map, Pelaaja kirjautunut, Peli peli) throws Exception {
         if(!tarkistaPaasy(kirjautunut, peli, map)){
             return;
+        }
+        System.out.println("OIKEA HENKILÖ");
+        
+        List<List<String>> kartta = peli.getUusinVuoro().getRuudut();
+        
+        for(List<String> rivi : kartta){
+            System.out.println("UUSI RIVI");
+            for(String ruutu : rivi){
+                System.out.println(ruutu);
+            }
         }
         
         map.put("kartta", peli.getUusinVuoro().getRuudut());
